1d725cc5bc14048cbf38b1fa9a766006f22e7d8f
Alex Kao <aexkao@gmail.com>
2024-12-27 19:13:55 -0500

    given zip file + maven dependencies installed

---
+++ restapi101/src/main/java/com/example/restapi101/Restapi101Application.java
@@ -0,0 +1,13 @@
+package com.example.restapi101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restapi101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restapi101Application.class, args);
+	}
+
+}
---
+++ restapi101/src/test/java/com/example/restapi101/Restapi101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapi101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restapi101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
